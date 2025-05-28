1399547322a563db7606704cb1647f4682117676
Madan Golay Tamang <madantamang939@gmail.com>
2025-05-28 17:14:29 +0545

    welcome, terms of service complete

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -1,5 +1,296 @@
-export default function Page(){
-    return(<>
-    <p>page</p>
-    </>)
-}
+import Ribbon from "@/components/Ribbon";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Disclaimer | SRIYOG Consulting",
+};
+const Disclaimer = () => {
+  return (
+    <>
+      <Ribbon name="Disclaimer" des="" />
+      <section className="lg:w-[1180px] mx-auto max-lg:container max-lg:px-3 mb-[45px]">
+        <div className="flex flex-col gap-5 bg-white p-5 border border-[#dbdbdb] ">
+          <p>Effective date:Tuesday, May 14, 2024</p>
+
+          <p>
+            Sriyog Consulting Pvt. Ltd. (also referred as “us”, “we” or “our”
+            throughout this policy content) operates
+            the&nbsp;www.sriyog.com&nbsp;website and mobile application
+            (hereinafter referred to as the “Service”). Our policies regarding
+            the collection, use, and disclosure of basic personal data while
+            using the service and the associated data are described in detail
+            here.
+          </p>
+
+          <p>
+            Using your data and feedback, we continue upgrading of service. By
+            using&nbsp;Sriyog.com, we assume that you have trusted and agreed to
+            the collection and use of information as per our policy. The terms
+            defined in this privacy policy have the same meaning as in our terms
+            and conditions unless especially redefined in this privacy policy
+            in&nbsp;www.sriyog.com.
+          </p>
+
+          <p>
+            <strong>Uses and collection of Information&nbsp;</strong>
+          </p>
+
+          <p>
+            Different types of information for various purposes we have
+            collected your data and it is always used for improving our service
+            just for you.
+          </p>
+
+          <p>
+            <strong>Different Types of Collected Datas</strong>
+          </p>
+
+          <p>Personal Information Data</p>
+
+          <p>
+            To create your identity, we offer you to create your profile by
+            providing your personally identifiable information that is later
+            used by other providers to connect you. Personally identifiable
+            information includes below, but not limited to:
+          </p>
+
+          <ul>
+            <li>Mobile Number</li>
+            <li>Email Address</li>
+            <li>First Name and Last name</li>
+            <li>Address , Province , City</li>
+            <li>Cookies and Usage Data</li>
+            <li>Skills and expertises&nbsp;</li>
+          </ul>
+
+          <p>Usage Information Data</p>
+
+          <p>
+            We may also collect information on how people use our services
+            (“Usage Data”). This Usage Data may include various information such
+            as your computer IP, browser details, pages of the services, pages
+            that you navigate, time stamp you stay on pages, your engagements,
+            UDIDs, etc.
+          </p>
+
+          <p>Tracking and Cookies Data</p>
+
+          <p>
+            Using cookies and similar tracking technologies, we track the
+            activities of our services and hold certain information.
+          </p>
+
+          <p>
+            Cookies are files with small amount of data which may include
+            anonymous unique identifiers and are sent to your browsers from a
+            website and stored on your devices. Tracking technologies use tags
+            and scripts to collect information to track helping to improve our
+            services.
+          </p>
+
+          <p>
+            Instructing your browser to refuse all cookies or monitoring the
+            cookies can be done. However, if you do not accept cookies, you may
+            not be able to use some portions of our services that may depend on
+            your available cookies.
+          </p>
+
+          <p>Examples of cookies we use:</p>
+
+          <ul>
+            <li>
+              <strong>Session Cookies</strong> – Session cookies are used to
+              operate our services.
+            </li>
+            <li>
+              <strong>Preference Cookies</strong> – Preference cookies are used
+              to remember your preferences and various settings.
+            </li>
+            <li>
+              <strong>Security Cookies</strong> –&nbsp; Security cookies are
+              used for security purposes.
+            </li>
+          </ul>
+
+          <p>
+            <strong>Uses of Data</strong>
+          </p>
+          <p>Sriyog.Com uses the collected data for various purposes:</p>
+
+          <ul>
+            <li>Providing and maintaining the service</li>
+            <li>Notifying you about changes to our service</li>
+            <li>
+              Letting you participate in interactive features of our service
+            </li>
+            <li>Providing customer care and support</li>
+            <li>
+              Providing analysis or valuable information so that we can improve
+              the service
+            </li>
+            <li>Monitoring the usage of the service</li>
+            <li>Detecting, preventing and addressing technical issues</li>
+          </ul>
+
+          <p>
+            <strong>Transfer Of Data</strong>
+          </p>
+
+          <p>
+            Your information, including Personal Data, may be transferred to the
+            server and maintained on server computers.
+          </p>
+
+          <p>
+            If you are outside Nepal and choose to provide information to us,
+            please understand that we transfer your data to our servers in Nepal
+            and process it there.
+          </p>
+
+          <p>
+            Your consent to this Privacy Policy along with your submission of
+            such information means that you agree with that transfer.
+          </p>
+
+          <p>
+            Sriyog Consulting Pvt. Ltd. will take all the necessary steps to
+            ensure that your data is secure and as per this Privacy Policy and
+            no transfer of your Personal Data to an organization or a country
+            will take place before ensuring the security of your data and other
+            personal information.
+          </p>
+
+          <p>
+            <strong>Disclosure Of Data</strong>
+          </p>
+
+          <p>
+            <strong>Legal Requirements</strong>
+          </p>
+
+          <p>
+            Sriyog Consulting Pvt Ltd may disclose your Personal Data in the
+            good faith belief that such action is necessary to:
+          </p>
+
+          <ul>
+            <li>Complying with a legal obligation</li>
+            <li>
+              Protecting and defending the rights or property of Sriyog
+              Consulting Pvt. Ltd&nbsp;
+            </li>
+            <li>
+              Preventing or investigating&nbsp; possible wrongdoing in
+              connection with the Service
+            </li>
+            <li>
+              Protecting the personal safety of users of the Service or the
+              public
+            </li>
+            <li>Protecting against legal liability</li>
+          </ul>
+
+          <p>
+            <strong>Security Of Data</strong>
+          </p>
+
+          <p>
+            Knowing the importance of security of your data is important to us,
+            but remember that no method of transmission over the Internet, or
+            method of electronic storage is 100% secure. We are always working
+            to protect your Personal Data, but we cannot guarantee its absolute
+            security.
+          </p>
+
+          <p>
+            <strong>Service Providers</strong>
+          </p>
+
+          <p>
+            In order to provide you our services, we may employ third party
+            companies and individuals (“Service Providers”), who may provide the
+            Service on our behalf, perform Service-related services or assist us
+            in analyzing how our Service is used.
+          </p>
+
+          <p>
+            To provide the services on our behalf, these third parties may have
+            access to your personal data but are obligated not to disclose or
+            use it for any other purpose.
+          </p>
+
+          <p>
+            <strong>Other Sites</strong>
+          </p>
+
+          <p>
+            Our Service may contain other sites links that are not operated by
+            us. If you click on a third-party link, we refer it as third party’s
+            site. We strongly advise you to review the Privacy Policy on that
+            site.
+          </p>
+
+          <p>
+            We have no control over such sites and take no responsibility for
+            the content they post, privacy policies or their practices.
+          </p>
+
+          <p>
+            <strong>Children’s Privacy</strong>
+          </p>
+
+          <p>
+            We do not address anyone under 18(“Children”). We knowingly do not
+            collect any personal details from anyone under 18, it is parental or
+            guardian’s duty to aware your children. If found sharing of the
+            personal data by children you can contact to us. If we become aware
+            that we have collected Personal Data from children without parental
+            verification, we shall take the necessary steps to remove
+            information from our servers.
+          </p>
+
+          <p>
+            <strong>Changes To This Privacy Policy</strong>
+          </p>
+
+          <p>
+            We regularly update our Privacy Policy from time to time. If any
+            revision on our Privacy Policy should take place, we will notify you
+            by posting it on this page.
+          </p>
+
+          <p>
+            We will let you know about the changes via email and/or a prominent
+            notice on our Service, before the actual changes becoming effective
+            and will also update the “effective date” at the top of this Privacy
+            Policy.
+          </p>
+
+          <p>
+            We recommend you to review this Privacy Policy periodically for any
+            changes. After posting on this page, Privacy Policy will be
+            effective from the date posted.
+          </p>
+
+          <p>
+            <strong>Contact Us</strong>
+          </p>
+
+          <p>
+            If you have any questions about this Privacy Policy, please contact
+            us:&nbsp;
+            <a
+              href="/cdn-cgi/l/email-protection"
+              className="__cf_email__"
+              data-cfemail="325b5c545d7241405b4b5d551c515d5f"
+            >
+              [email&#160;protected]
+            </a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Disclaimer;
---
+++ app/tos/page.tsx
@@ -0,0 +1,200 @@
+import Ribbon from "@/components/Ribbon";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terms of Services | SRIYOG Consulting",
+};
+const Tos = () => {
+  return (
+    <>
+      <Ribbon name="Terms of Services" des="" />
+      <section className="mb-[45px] mx-auto max-lg:container max-lg:px-3 lg:w-[1180px] max-md:shadow-none border max-md:border-none border-[#dbdbdb] ">
+        <div className=" p-[55px] max-md:p-0 ">
+          <p className="mb-[15px] text-[18px]">
+            Last updated : 1<sup>st</sup>&nbsp;June, 2024
+          </p>
+          <p className=" mb-[10px]">
+            Welcome to SRIYOG. Please read these Terms and Conditions (“Terms”,
+            “Terms and Conditions”) carefully before using the website/ mobile
+            application/ social media platforms (the “Service”) operated by
+            SRIYOG Consulting Pvt. Ltd. (“us”, “we”, or “our”).
+          </p>
+          <p className=" mb-[10px]">
+            SRIYOG may change or amend the Terms as per the requirement in the
+            future by posting revisions here. You are allowed to use the
+            Services on the basis of your acceptance of and compliance with
+            these Terms. These Terms are applicable to all the visitors,
+            professionals, and users who access or use the Service.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            If you are accessing or using the service, it means that you agree
+            to be bound by these Terms. If you disagree with any part of the
+            terms, then we advise you to immediately stop using the service.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Eligibility/Age</p>
+          <p className="mb-[56px]  max-md:mb-[20px]">
+            Anyone under the age of 18 is not authorized to provide or receive
+            services in SRIYOG
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Accounts</p>
+          <p className=" mb-[10px]">
+            You are supposed to provide us with information that is accurate,
+            complete, and current at all times when creating an account in
+            SRIYOG. If the provided information is misleading or incorrect, it
+            will be considered a breach of the Terms, which may result in
+            immediate termination of your account on our Service.
+          </p>
+          <p className=" mb-[10px]">
+            You are yourself responsible to safeguard your account details like
+            username and password.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            You agree to keep your password safe and not to share it to any
+            third party. If your account is compromised or in case of any
+            security issue, you must notify us immediately.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">
+            Understanding of our Platform
+          </p>
+          <p className=" mb-[10px]">
+            SRIYOG is a bridge between the service seekers and service providers
+            and facilitates the communication between them with mobile
+            application, website, social media or any digital medium. We aim to
+            increase self-employment and control the problem of unemployment
+            through our platform.
+          </p>
+          <p className=" mb-[10px]">
+            We do not endorse or promote any of the information or services
+            provided by the users. These users (including you) or any third
+            party are independent contractors and are not related or affiliated
+            to us in any way. As such, you agree not to hold us responsible for
+            the violation, error, carelessness or breach of any of the Service
+            or information listed on our website or mobile application.
+          </p>
+          <p>
+            Furthermore, we do not provide any insurance or assurance to any of
+            the services listed on our website, neither we will charge any fees
+            for using our platform. If you are using SRIYOG for providing or
+            receiving any service from our users, we strongly recommend you to
+            take precautions and safety measures to protect yourself.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            You agree that you cannot hold us responsible in case of any
+            defaults from the providers. If you have any bad experience, please
+            complain us about the particular Provider and we will take action to
+            terminate their accounts.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">
+            Links to Other Web Sites
+          </p>
+          <p className=" mb-[10px]">
+            Our site may contain links to third-party web sites or services that
+            are not owned or controlled by SRIYOG Consulting Pvt. Ltd.
+          </p>
+          <p className=" mb-[10px]">
+            SRIYOG Consulting Pvt Ltd has no control over, and assumes no
+            responsibility for, the content, privacy policies, or practices of
+            any third-party web sites or services. You further acknowledge and
+            agree that SRIYOG Consulting Pvt. Ltd. shall not be responsible or
+            liable, directly or indirectly, for any damage or loss caused or
+            alleged to be caused by such content, goods or services available on
+            or through any such web sites or services.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            As such websites might have their own terms and condition unaware to
+            us, we strongly recommend you to read their terms and conditions and
+            privacy policies of such third-parties you visit.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Delivery/Payment</p>
+          <p className=" mb-[10px]">
+            SRIYOG does not deliver any service or accepts fees from the users
+            for any of the listed services. We are not involved in providing or
+            receiving services nor act as a broker, agent or middleman between
+            the Provider and Seekers.
+          </p>
+          <p className=" mb-[10px]">
+            Providers are themselves responsible to determine the charges,
+            deliver their service, obtain the payment or create invoices for
+            their services. Likewise, the Service Seekers are also themselves
+            responsible for the negotiation of the charges or verify the Service
+            before releasing any payments.
+          </p>
+          <p className=" mb-[10px]">
+            By using our service, both the Providers and Seekers agree to take
+            the necessary measures regarding the transaction and obtain
+            appropriate documents related to the dealing like any other offline
+            transactions.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            Any contract, warranties, terms or conditions associate between the
+            Providers and Seekers are limited solely between them and SRIYOG is
+            not liable for such contracts in any manner.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">
+            Copyrights/Intellectual Property Rights:
+          </p>
+          <p className=" mb-[10px]">
+            All the images, logo, text and content in our site are either your
+            content, the content of SRIYOG or of the other users. You agree that
+            they are the intellectual property of the respective owners and you
+            shall not copy, modify, or distribute the content that is not owned
+            by you.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            Such contents are protected by the Intellectual Property Laws of
+            Nepal Government and other international laws. You will be held
+            liable in case of infringement of copyright.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Termination</p>
+          <p className=" mb-[10px]">
+            If you breach our Terms, we may immediately terminate or suspend
+            access to your account with or without prior notice. Upon
+            termination, you will immediately lose access to our Service and
+            your right to use our service will cease.
+          </p>
+          <p className=" mb-[10px]">
+            If you voluntarily wish to terminate your account, you may contact
+            us or simply discontinue using the Service.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            Even after the termination of your account, you will be liable for
+            the breach of the contract and legal actions may be taken against
+            you.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Governing Law</p>
+          <p className=" mb-[10px]">
+            If we fail to enforce any right or provision of these Terms, it does
+            not mean that you have been provided a waiver of those rights.
+          </p>
+          <p className=" mb-[56px] max-md:mb-[20px]">
+            If any provision of our Terms is in conflict with the Law of Nepal
+            and held to be invalid or unenforceable by a court, the remaining
+            provisions of these Terms will remain in effect. These Terms are the
+            only terms between us that is applicable regarding our services and
+            supersede and replace any other agreements prior to thus that we
+            might have between us regarding the service.
+          </p>
+          <p className="text-[1.4rem] font-bold mb-[10px]">Amendments</p>
+          <p className=" mb-[10px]">
+            SRIYOG reserves the right, at its sole discretion, to amend or
+            change these Terms at any time. If we find a revision necessary, we
+            will notify you at least 15 days-notice prior to any new terms
+            taking effect.
+          </p>
+          <p className=" mb-[10px]">
+            The subject of changes in terms will be determined at our sole
+            discretion.
+          </p>
+          <p className=" ">
+            If you continue to use our Service after those changes become
+            effective, it means that you agree to be abided by the revised
+            terms. You should immediately cease to use the service if you do not
+            agree to the new terms.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Tos;
