4608baafcd14a80a02613378f3b7d63d3e93e233
Youndhen <youndyt929@gmail.com>
2025-06-21 13:59:55 +0545

    problem solved

--- app/blog/pegasus-sypyware/page.tsx
+++ app/blog/pegasus-sypyware/page.tsx
@@ -1,17 +1,19 @@
-import Link from "next/link";
+
 import Image from "next/image";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Pegasus Spyware – You Are Hacked",
 };
+
 export default function psrh() {
   return (
     <>
-      <section className=" lg:w-[900px] max-lg:container max-lg:px-3 mx-auto my-[45px] space-y-6">
-        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+      <section className="lg:w-[900px] max-lg:container max-lg:px-3 mx-auto my-[45px] space-y-6">
+        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/blog/5.jpg"
-            alt="essesntial-things-to-keep-on-website"
+            alt="essential-things-to-keep-on-website"
             className="object-cover h-full w-full"
             fill
           />
@@ -25,33 +27,32 @@ export default function psrh() {
           </p>
           <p className="text-center italic">Published on: March 9, 2025</p>
         </div>
+
         <p>
-          {" "}
           Flashback of just two decades in Nepal, it was hard to see people
           carrying bar phones. The dream of people was not a smart phone but
-          basic one. NOKIA, Ericsson, Sony etc were market leader and the most
-          important factor was a basic connectivity, upgrading from pager. GSM
+          basic one. NOKIA, Ericsson, Sony etc were market leaders and the most
+          important factor was basic connectivity, upgrading from pager. GSM
           network was not much penetrated and the phones which values USD 15 at
           current dates were much valued just for voice and text.
         </p>
 
         <p>
-          {" "}
           A mixture of Pager and Landline, at that time was known as Cell phone
           or phone with mobility synonym as Mobile phone and till now most of
-          the people still call as "Mobile" which uses cellular network and its
-          full name is "Mobile Phone or Cell Phone"
-        </p>
-        <p>
-          {" "}
-          Time increased and technology was emerging, the luxury phone became
-          necessity day by day and it increased its presence and till date we
-          have wide ranges of options to upgrade from basic bar phone with
-          minimal features to high end, secured, sophisticated phone which are
-          difficult to fit in the pocket{" "}
+          the people still call it "Mobile" which uses cellular network and its
+          full name is "Mobile Phone or Cell Phone".
         </p>
 
-        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+        <p>
+          Time increased and technology was emerging, the luxury phone became
+          necessity day by day and it increased its presence. Till date, we
+          have wide ranges of options to upgrade from basic bar phone with
+          minimal features to high end, secured, sophisticated phones which are
+          difficult to fit in the pocket.
+        </p>
+
+        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/blog/spyware-2.jpg"
             alt="uiux-design"
@@ -59,48 +60,47 @@ export default function psrh() {
             fill
           />
         </div>
+
         <p>
-          {""}
-          The evolution of mobile phone was for communication on early days but
-          after 2010, mobile phone became smart phone and it became necessity to
-          have phone either for luxury or for showcasing. Bar phones are hardy
-          seen in pocket. We are in such generation where we have wrong belief
-          that carrying smart phone makes people smart or s/he is more valuable
-          with brands such as iPhone/ Samsung/ One Plus and hundreds of more
-          easily available in market packed with Android, iOs and so one.
+          The evolution of mobile phone was for communication in early days but
+          after 2010, mobile phones became smart phones and it became a
+          necessity to have phone either for luxury or for showcasing. Bar
+          phones are hardly seen in pockets. We are in such a generation where
+          we have wrong belief that carrying a smart phone makes people smart
+          or s/he is more valuable with brands such as iPhone, Samsung, OnePlus
+          and hundreds more easily available in market packed with Android,
+          iOS, and so on.
         </p>
 
         <p>
-          {""}
           The more we upgraded from Voice and Text based phones with tons of
-          Apps widely available in market we have more risk. RAT is a system
-          which can easily installed in victim's laptop or desktop or even
-          servers to get access their screenshots, data, files and almost a
-          person can operate the laptop/ computer from any corner of the world
-          with a basic system.
+          apps widely available in market, the more risk we have. RAT is a
+          system which can be easily installed in victim's laptop, desktop or
+          even servers to get access to their screenshots, data, files and
+          almost a person can operate the laptop/computer from any corner of
+          the world with a basic system.
         </p>
 
         <p>
-          {""}
-          Coming to the recent spyware surveillance named as "Pegasus spyware"
-          now smart phones users are aware that their end to end encryption is
-          not fully encrypted or any one can install spyware system in their
+          Coming to the recent spyware surveillance named as "Pegasus spyware,"
+          now smartphone users are aware that their end-to-end encryption is
+          not fully encrypted or anyone can install spyware system in their
           mobile phones not only by sending links in WhatsApp but also the
-          spyware was easily installed in phones with a Blank Calls to victims
+          spyware was easily installed in phones with blank calls to victims
           phones from WhatsApp.
           <br />
           <br />
-          "As most of the people are aware about "Telegram, Discord, Signal"
-          which are synonym of the most secured chat applications are not
-          secured."
+          "As most people are aware, 'Telegram', 'Discord', 'Signal' which are
+          synonyms of the most secured chat applications are not secured."
           <br />
           <br />
           Android has basic limitations as compared to iOS for publishing apps
-          in App Store. Blackberry was one of the best and secured system which
-          was managed by Research In Motion, a Canadian firm which as managing
+          in App Store. Blackberry was one of the best and secured systems
+          which was managed by Research In Motion, a Canadian firm managing
           servers in Canada.
         </p>
-        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+
+        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/blog/spyware-3.jpg"
             alt="uiux-design"
@@ -110,23 +110,23 @@ export default function psrh() {
         </div>
 
         <p>
-          {""}
           In many countries, Government itself made policy to remove Blackberry
-          services and only handsets were used till few years back and now in
-          2021 it is very hard to find Blackberry(even without Blackberry
+          services and only handsets were used till few years back. Now in 2021
+          it is very hard to find Blackberry (even without Blackberry
           services).
           <br />
           <br />
-          Lots of Messaging app provides have been struggling in market branding
-          their app with secured, most secured, end to end encryption which
-          seems to failed now. People named{" "}
-          <span className="font-[800]">Pegasus</span> as spyware few days back
-          after knowing it was collecting the data from various Journalist,
-          Governments, Politicians, Bureaucrats etc but till now many people are
-          unaware that what might WhatsApp, Viber, IMO, TikTok, ClubHouse,
-          YouTube are collecting their data.
+          Lots of messaging app providers have been struggling in market branding
+          their app as secured, most secured, end-to-end encrypted which seems
+          to have failed now. People named{" "}
+          <span className="font-[800]">Pegasus</span> as spyware a few days back
+          after knowing it was collecting data from various journalists,
+          governments, politicians, bureaucrats etc. But till now many people
+          are unaware of what WhatsApp, Viber, IMO, TikTok, ClubHouse, YouTube
+          are collecting from their data.
         </p>
-        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+
+        <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/blog/spyware-4.png"
             alt="uiux-design"
@@ -134,28 +134,29 @@ export default function psrh() {
             fill
           />
         </div>
+
         <p>
-          Google Maps access everyone's location, YouTube is built in such
-          algorithm which collects and measures users interests and shows the
-          related videos and a person is forced to watch whole night killing his
-          or her time. NSO Group, the company behind Pegasus ( so called
-          Spyware) was founded by Niv Carmi, Shalev Hulio & Omri Lavie founded
-          NSO Group in 2010 and their website www.nsogroup.com and mentioning
+          Google Maps access everyone's location, YouTube is built with an
+          algorithm which collects and measures users' interests and shows
+          related videos. A person is forced to watch whole night killing his or
+          her time. NSO Group, the company behind Pegasus (so-called spyware),
+          was founded by Niv Carmi, Shalev Hulio & Omri Lavie in 2010. Their
+          website is www.nsogroup.com and mentioning
           <br />
           <br />
           <span className="text-xl text-gray-500">
-            "HELPING GOVERNMENTS MAINTAIN PUBLIC SAFETY "
+            "HELPING GOVERNMENTS MAINTAIN PUBLIC SAFETY"
           </span>
           <br />
           <br />
-          From NSO Group Website : "NSO Group, develops best-in-class technology
-          to help government agencies detect and prevent a wide-range of local
+          From NSO Group Website: "NSO Group develops best-in-class technology
+          to help government agencies detect and prevent a wide range of local
           and global threats. Our products help government intelligence and
           law-enforcement agencies use technology to meet the challenges of
           encryption to prevent and investigate terror and crime."
         </p>
 
-        <div className="relative w-full md:h-[1200px] h-[750px] rounded-3xl overflow-hidden ">
+        <div className="relative w-full md:h-[1200px] h-[750px] rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/blog/spyware-5.jpg"
             alt="uiux-design"
@@ -165,23 +166,23 @@ export default function psrh() {
         </div>
 
         <p>
-          In this digital era, most of the everything is contented with
-          internet, not only our laptop, desktops or smart phone but also cars,
-          trains, microwave and hundreds of devices are connected with internet
-          with we give name as IoT.
+          In this digital era, most everything is connected with internet, not
+          only our laptops, desktops or smartphones but also cars, trains,
+          microwaves and hundreds of devices connected to the internet which we
+          call IoT.
           <br />
           <br />
           <span className="text-xl text-gray-500">
-            "The good and bad things about internet is "EVERYTHING IS
-            CONNECTED."
+            "The good and bad thing about internet is 'EVERYTHING IS
+            CONNECTED.'"
           </span>
           <br />
           <br />
-          Now a days, a device, social media, social messaging, a camera,
-          instagram, Viber or anything which we have been using are like drugs,
-          which changes our habits, we are killing productive time which leads
-          dark future and also it helps to compromise data. The more app you
-          install, the more you are in risk.
+          Nowadays, a device, social media, social messaging, a camera,
+          Instagram, Viber or anything we use are like drugs, which change our
+          habits. We are killing productive time which leads to a dark future
+          and also helps to compromise data. The more apps you install, the more
+          you are at risk.
         </p>
       </section>
     </>
