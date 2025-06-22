414fee4adb0e410261f63ed2dad2e04c9e8f3397
Youndhen <youndyt929@gmail.com>
2025-06-22 21:22:52 +0545

    half blog pages

---
+++ app/blog/business-model-canvas/page.tsx
@@ -0,0 +1,165 @@
+import Image from "next/image"
+import Link from "next/link"
+export default function BusinessModelPage(){
+  return(
+    <>
+    <section className=" lg:w-[900px] max-lg:container max-lg:px-3 mx-auto my-[45px] space-y-6">
+                    <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+                      <Image
+                        src="/assets/images/blog/business-1.jpg"
+                        alt="things-to-keep-in-website"
+                        className="object-cover h-full w-full"
+                        fill
+                      />
+                    </div>
+                    <div>
+                      <p className="text-center">Sriyog.Com Business Model Canvas
+
+</p>
+                      <h1 className="font-extrabold text-4xl text-[#055d59] text-center">
+   Business Model Canvas
+
+                      </h1>
+                      <p className="text-center italic">
+        By: Pracas Upreti, Information Technology Consultant
+                      </p>
+                      <p className="text-center italic">August 12, 2019
+
+
+    
+    
+        
+        </p>
+                    </div>
+
+                    <p>The man behind Business Model Canvas was initially proposed by Alexander Osterwalder based on his business model ontology. Which has nine business modelling blocks. A business wants to be more great business, Jeff Bezos wants to earn more and more. A student wants to become a businessman.</p>
+
+                    <p>
+                      Business Model Canvas is used by well-established business to expand or scale where lean canvas model is used by startups to launch their products. No matters either it is for billion-dollar business of 100 dollar startup, the thousand miles of journey starts with the first step where travelling from lean canvas to business model canvas will result the brighter sunshine.
+                    </p>
+
+                    <p>No alt text provided for this image Starting business seems easy, register your business, make social media presence, work day and night, no proper plans no goals. The report says that 80% of business fails in first 2 years. Rest struggles, survives and becomes global brand. A famous mathematician says that “The business needs to execute 1000 days, which makes 1000 people to know the business which will be flourished in those 1000 days.”</p>
+                    <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+                      <Image
+                        src="/assets/images/blog/business-2.jpg"
+                        alt="things-to-keep-in-website"
+                        className="object-cover h-full w-full"
+                        fill
+                      />
+                    </div>
+
+                    <p>
+                      Business history shows that there was <Link className="font-[900] text-blue-600" href="https://en.wikipedia.org/wiki/Barter">barter system</Link> in business where someone use to exchange rice with salt. Remembering before 50 years there was no Amazon, no UBER, no Apple and no Samsung. Everything was going on in simple way where business use to survive only. There was nothing called expansion in major people. The generation of technology reached in such boom that you can order anything from US by sitting any corner of the world.
+
+                    </p>
+
+                    <p>
+                      Assuming bit earlier; there would be a man who thought people are ready to get delivery in any cost. A person may be dreaming Italian Ferrari being in China, a women thinking about American Rice Cooker from a far village in India. The company planned to deliver anywhere. The Amazon started their business by selling books online which now made Jeff Bezos world&apos;s greatest ecommerce gateway. The delivery system served to amazon and technology company serving them both by providing software, security, parcel tracking mobile application.
+
+                    </p>
+
+                    <p>
+                      No alt text provided for this image It is not only Amazon has succeeded, the race has many great minds behind the great mind. Google & FlipKart duo are always ahead, UBER beating marketing, virgin ideas are rocking virgin minds. The first runner is far behind the fast runner.
+
+                    </p>
+
+                    <p>There are 4 major things in professional life, that&apos;s job, freelance, business & startup. Like everyone needs the best resume to get the best job; for every business & startup Business Model Canvas is necessary which functions like spinal cord of body.
+
+</p>
+
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+                      <Image
+                        src="/assets/images/blog/business-3.jpg"
+                        alt="things-to-keep-in-website"
+                        className="object-cover h-full w-full"
+                        fill
+                      />
+                    </div>
+                    <p>All companies from UBER to Google has their own business model canvas where they have 9 major system of business who now are leading the world.
+
+</p>
+
+<b>
+  Business model canvas has 9 elements which equally has their role.
+
+</b>
+
+<ul>
+  <li>
+    <span className="font-[900]">Key Patners &mdash;</span>The pitch deck must fit multiple operating system, version of Windows, Mac or any other targeted devices.
+  </li>
+
+    <li>
+    <span className="font-[900]">Key Activities &mdash;</span>The activity which solves or improve the process and that engages in creating values. This can be distribution network. This minimizes the costing for customers and business.
+  </li>
+
+    <li>
+    <span className="font-[900]">Key Resources &mdash;</span>The resources needed to acquire in delivering value which can be raw materials, intellectual, workforce or in financial term.
+  </li>
+      <li>
+    <span className="font-[900]">Cost Structure &mdash;</span>Operating expenses which is fixed cost and the cost of goods sold, service deliver is variable cost. The cost that is paid for rent & utilities. This cost goes up when the business is increased as labor hours, materials, security, system etc.
+  </li>
+      <li >
+    <span className="font-[900]">Value Proposition &mdash;</span>Creating & adding value to compete in competitive market. The things which makes products unique or better than any other releases in market.
+  </li>
+      <li>
+    <span className="font-[900]">Channels &mdash;</span>The pitch deck must fit multiple operating system, version of Windows, Mac or any other targeted devices.
+  </li>
+
+        <li>
+    <span className="font-[900]">Customer Segments &mdash;</span>The area, a group or a group of people who are interested or those people who knows the value of the product. Facebook has great customers segment rather than a local restaurant.
+  </li>
+
+
+        <li>
+    <span className="font-[900]">Revenue Streams &mdash;</span>The last but not least, which manages the graph of the business. The ways how business/ startup/ product can generate income. This can be subscription fee, consultancy charges, licensing, usage fee, royalty, how business can grow or sustain.
+  </li>
+
+
+
+</ul>
+
+<p>At the starting Business Model Canvas makes feel like strange so the major thing is to understand it in very easy way. Business Model Canvas is rocket science for business. It doesn’t give financial projection, report, breakeven point but makes to meet all the goals and target. Starting from who can be a customer to how the business/ startup will function which has been crystalize to better understand how to start business or how to rescale business.
+
+</p>
+
+<p>The things are necessary to know major things by every entrepreneur, businessman and anyone who wants to leave the job and start anything in their own who thinks that freedom is everything in life which can give to work in free environment and make changes in lives & community.
+
+</p>
+
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+                      <Image
+                        src="/assets/images/blog/business-4.jpg"
+                        alt="things-to-keep-in-website"
+                        className="object-cover h-full w-full"
+                        fill
+                      />
+                    </div>
+
+<p className="text-2xl text-gray-500">
+  All about revenue streams :
+
+</p>
+                    <p>Business model Canvas’s 9th number or almost mentioned as goal mentions the financial profitability part. When someone starts business, there is a mistake of thinking that there is no doubt of being failure. All the way like roadmap “Business Model Canvas” plans everything from beginning to scaling business. A business needs to make a turn in the right way to drive their business in expressway. Amazon, Google, UBER, Hyperloop, Tesla are moving in expressway.
+
+</p>
+
+<p>No only technology, internet, social media also has turned them big but the things are doing the right thing in right time with right business modelling, planning and implementing to execute the best. The cream products of technologies are connectivity. Like a flight from Delhi to New York makes connectivity. UBER connects drivers with riders, Google connects with an information searcher by showing the results searching billion of pages from their database (big data).
+
+</p>
+
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+                      <Image
+                        src="/assets/images/blog/business-5.jpg"
+                        alt="things-to-keep-in-website"
+                        className="object-cover h-full w-full"
+                        fill
+                      />
+                    </div>
+                    <p>Download Sample Canvas : <Link href='https://www.strategyzer.com/library/the-business-model-canvas' className="text-blue-600 font-[900]">Click Here</Link> | Direct Download from Canvanizer : <Link href='https://canvanizer.com/downloads/business_model_canvas_poster.pdf' className="text-blue-600 font-[900]">Click Here</Link>
+
+</p>
+                    </section>
+    </>
+  )
+}
---
+++ app/blog/story-behind-the-airbnb-story/page.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image"
+export default function Airbnb(){
+  return(
+    <>
+    <section className=" lg:w-[900px] max-lg:container max-lg:px-3 mx-auto my-[45px] space-y-6">
+            <div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+              <Image
+                src="/assets/images/blog/airbnb-1.jpg"
+                alt="things-to-keep-in-website"
+                className="object-cover h-full w-full"
+                fill
+              />
+            </div>
+            <div>
+              <h1 className="font-extrabold text-4xl text-[#055d59] text-center">
+Story behind &quot;the airbnb story&quot;​ book
+              </h1>
+              <p className="text-center italic">
+By: Pracas Upreti, Information Technology Consultant
+              </p>
+              <p className="text-center italic">Published on: March 9, 2020
+
+</p>
+            </div>
+            <p>
+              People worked for vision not to change the society but to make a Digital Empire to print money, create opportunities and inject few things which disrupted an industry but not the thoughts and even feelings.
+            </p>
+
+            <p className="text-2xl text-gray-500 font-[900]">"Zero (0) too has value if it stands after 1 to 9, if its alone it always becomes a big ZERO."
+            </p>
+            <p>A business must be bounded by feelings, emotions and thoughts to change a society. We created thousands to billion dollars of empire, business, startups and a system where people can simply earn from few clicks in his/her smartphones. The use of technology has been in rapid growth stage that there are thousands of ways to earn, learn and lead the life easily. Technology has helped in many ways to get connected, either it is to become personal, professional or social. Thousands of Mobile Applications, different platforms, Gigabytes of storages and many megapixels of front and back camera.
+</p>
+
+<p>Airbnb started with a simple idea to share their homes, unused spaces, a airbed and hence billion dollar business. A true statement says that &#34;
+Thousands of mile journey starts with a first step&#34;
+ where they themselves tried, where the idea was in mind, a simple thing, available resources and the simple website based on WordPress. Focused for International&#39;
+s Design Conference in 2007 Brain Chesky and his co-founder Joe Gebbia started renting their apartment with Air-Mattress which is now billion dollar business. The design conference which was held on October 2007 overcrowded city hotels so they say the opportunities to start something and solving something.
+
+</p>
+
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+              <Image
+                src="/assets/images/blog/airbnb-2.jpg"
+                alt="things-to-keep-in-website"
+                className="object-cover h-full w-full"
+                fill
+              />
+            </div>
+            
+            <p>Airbnb founders Joe Gebbia, Nathan Blecharczyk, and Brian Chesky. : Source Airbnb
+<br /><br />
+The company now has moved very ahead than HomeAway, VRBO etc. Crossing different and difficult turns, now it has kept its name on the top of home sharing business. The early stage was difficult where they e-mailed design blog and conference organizers asking them to help promoting their website which they supported.
+<br /><br />
+It was hard to let a strangers staying and allocating own room but the things went smooth where people had to trust only by seeing profile, some registered and recorded information. <span className="font-[900]">Technology helps to build better technology</span>. So the Airbnb did to establish and implement new ideas, verification system, beautiful photography, easy to use system and more which they faced as obstacle in their success and making dream come true. Where they once had said that once people will read a book about Airbnb which we have been reading it. They made a list of companies whose cultures they wanted to emulate.</p>
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+              <Image
+                src="/assets/images/blog/airbnb-3.jpg"
+                alt="things-to-keep-in-website"
+                className="object-cover h-full w-full"
+                fill
+              />
+            </div>
+
+            <p>2013 was the year when Airbnb introduced ID Verification system which enhanced identity proofs and included more proof of identity where both hosts and guests have the options to only do business with verified id. The team of data scientists, who created behavioral models to help identify, used machine learning to develop tools that analyze reservations to help detect risk.
+<br /><br />
+<span className="text-lg font-[900]">&#34;
+Early to bed, early to rise, and work like hell and organize&#34;
+ - Chris Lehane
+</span>
+</p>
+
+<div className="relative w-full md:h-[450px] h-[250px] rounded-3xl overflow-hidden ">
+              <Image
+                src="/assets/images/blog/airbnb-4.jpg"
+                alt="things-to-keep-in-website"
+                className="object-cover h-full w-full"
+                fill
+              />
+            </div>
+            <p>
+              Paris, Barcelona, and Santa Monica, Ca. have some of the strictest policies regarding who can and can&#39;
+t rent through Airbnb, while Amsterdam, Berlin, London, San Francisco, and New York have looser requirements. Though a billion dollar company, trusted by million people, had been growing like rocket, Airbnb is banned in many cities, areas and apartments. Apart form location ban, Airbnb has a process where they bans their host account by reviewing them. The suspension of account is managed & monitered according to the standards like how they have been maintaining. It also bans if a host has multiple accounts, ratings by guests and infringed terms of service.
+
+            </p>
+            </section>
+    </>
+  )
+}
