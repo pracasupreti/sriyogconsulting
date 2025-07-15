ad52fe6e48e228039be8eafc44a8982f1aadb980
Youndhen <youndyt929@gmail.com>
2025-07-15 10:33:16 +0545

    meet and patnership page fix

--- src/lib/defination.ts
+++ src/lib/defination.ts
@@ -1,6 +1,14 @@
-type referForm = {
-  orgaizationName :string,
-  email:string;
-  phone:string;
-  
+export interface PartnershipFormValues {
+  organizationName: string;
+  organizationEmail: string;
+  website: string;
+  city: string;
+  country: string;
+  personalName: string;
+  personalEmail: string;
+  designation: string;
+  message: string;
+  reason: string;
+  phoneOrganization: string;
+  personalPhone: string;
 }
